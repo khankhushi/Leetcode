46254916229ae6e9de76b8f30423f163c49c1bb9
Khushi Khan <81975567+khankhushi@users.noreply.github.com>
2022-06-22 16:22:33 +0530

    Time: 91 ms (6.47%), Space: 15.1 MB (16.77%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,17 +1,6 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        i=1
-        answer = []
-        for i in range(1,n+1):
-            if not i % 3 and not i % 5:
-                answer.append("FizzBuzz")
-            elif not i % 3 :
-                answer.append("Fizz")
-            elif not i % 5  :
-                answer.append("Buzz")
-            else :
-                answer.append(str(i))
-                
-        return answer
+        output=["Fizz" * (not i % 3) + "Buzz" * (not i % 5) or str(i) for i in range(1,n+1)]
+        return output
                 
         
