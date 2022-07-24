58dbb75fdf2ac459437fc07dad58ebdf7a661ecf
Khushi Khan <81975567+khankhushi@users.noreply.github.com>
2022-07-24 23:07:58 +0530

    Time: 6461 ms (10.84%), Space: 14.9 MB (95.36%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,13 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, n in enumerate(nums):
-            if target - n in d:
-                return nums.index(target-n), i
-            else:
-                d[n] = i
-        return nums.index(target-n), i
-            
-         
-            
-                
+        for i in range(0,len(nums)):
+            # for j in range (0,len(nums)):
+            for j in range (i+1,len(nums)): # Since we have already traversed till i and are finding a pair ahead
+                if target == nums[j] + nums[i]:
+                    return i, j
