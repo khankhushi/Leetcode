910ed081e4c26fe462cfa3bb85e6819c0823121b
Khushi Khan <81975567+khankhushi@users.noreply.github.com>
2022-12-12 20:34:40 +0530

    Time: 44 ms (72.47%), Space: 13.8 MB (58.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,22 @@
 #??
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        dic = [-1 for i in range(n)]
-        dic[0], dic[1] = 1, 2
-        return self.helper(n-1, dic)
+#     def climbStairs(self, n: int) -> int:
+#         if n == 1:
+#             return 1
+#         dic = [-1 for i in range(n)]
+#         dic[0], dic[1] = 1, 2
+#         return self.helper(n-1, dic)
 
-    def helper(self, n, dic):
-        if dic[n] < 0:
-            dic[n] = self.helper(n-1, dic)+self.helper(n-2, dic)
-        return dic[n]
+#     def helper(self, n, dic):
+#         if dic[n] < 0:
+#             dic[n] = self.helper(n-1, dic)+self.helper(n-2, dic)
+#         return dic[n]
+
+    def __init__(self):
+        self.dic = {1:1, 2:2}
+
+    def climbStairs(self, n):
+        if n not in self.dic:
+            self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dic[n]
 
