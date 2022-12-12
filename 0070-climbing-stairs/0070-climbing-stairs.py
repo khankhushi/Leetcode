e5e3ac31116ff5744000bca542551fcc3cf28133
Khushi Khan <81975567+khankhushi@users.noreply.github.com>
2022-12-12 20:29:00 +0530

    Time: 39 ms (78.60%), Space: 13.8 MB (96.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+#??
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        dic = [-1 for i in range(n)]
+        dic[0], dic[1] = 1, 2
+        return self.helper(n-1, dic)
+
+    def helper(self, n, dic):
+        if dic[n] < 0:
+            dic[n] = self.helper(n-1, dic)+self.helper(n-2, dic)
+        return dic[n]
+
