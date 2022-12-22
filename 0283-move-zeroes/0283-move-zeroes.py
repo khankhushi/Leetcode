df50233689f83e9bb09e302015045be81d508a7e
Khushi Khan <81975567+khankhushi@users.noreply.github.com>
2022-12-22 15:10:10 +0530

    Time: 174 ms (88.79%), Space: 15.6 MB (65.09%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,19 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         nums.remove(0)
+        #         nums.append(0)
+                
+        nonZeroCount = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+               nums[nonZeroCount], nums[i] = nums[i], nums[nonZeroCount]
+               nonZeroCount += 1
+            
+       
+            
+            
