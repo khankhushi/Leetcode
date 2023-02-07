a38ae3cfaa3cd95d412caf884e46c10ba1c87ea0
Khushi Khan <81975567+khankhushi@users.noreply.github.com>
2023-02-07 09:46:33 +0530

    Time: 249 ms (5.55%), Space: 13.8 MB (94.55%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        lst=[] 
+        for i in s: 
+            if '|' not in lst: 
+                lst.append(i) 
+            elif '|' in lst and i=='|': 
+                lst.pop() 
+        return lst.count('*')
+     
