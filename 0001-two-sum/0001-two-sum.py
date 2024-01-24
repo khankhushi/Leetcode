61496403dc329d43c776451561c13fd4bb80aa11
Khushi Khan <khushikhan2810@gmail.com>
2024-01-24 15:50:46 +0530

    Time: 65 ms (60.77%), Space: 17.9 MB (58.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,33 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        res = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j]== target and i!=j:
-                    res.append(i)
-                    res.append(j)
-                else:
-                    continue
-        return res
+        # res = []
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j]== target and i!=j:
+        #             res.append(i)
+        #             res.append(j)
+        #         else:
+        #             continue
+        # return res
         
+        
+        # idx = 0
+        # for i in range(len(nums)):
+        #     complement = target-nums[i]
+        #     if complement in nums:
+        #         idx = nums.index(complement)
+        #     ## condition that same element can't be used twice:
+        #     if idx == i:
+        #         continue
+        #     break
+        # return idx,i
+        
+        
+        # using dictionary
+        stre = dict()
+        for i in range(len(nums)):
+            comp = target-nums[i]
+            if comp in stre:
+                return [stre[comp], i]
+            else:
+                stre[nums[i]] = i
